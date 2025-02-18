ece2ff8124ad982297813aaf3cf227dce481d0c8
Thomas Nielsen <thomas.nielsen3805@gmail.com>
2025-02-18 17:38:22 -0600

    file adding

---
+++ src/classes/Motorbike.ts
@@ -0,0 +1,58 @@
+// Importing Vehicle and Wheel classes
+import Vehicle from './Vehicle.js';
+import Wheel from './Wheel.js';
+
+// TODO: The Motorbike class should extend the Vehicle class
+class Motorbike extends Vehicle {
+  // TODO: Declare properties of the Motorbike class
+  // TODO: The properties should include vin, color, make, model, year, weight, top speed, and wheels
+  // TODO: The types should be as follows: vin (string), color (string), make (string), model (string), year (number), weight (number), topSpeed (number), wheels (Wheel[])
+
+  vin: string;
+  color: string;
+  make: string;
+  model: string;
+  year: number;
+  weight: number;
+  topSpeed: number;
+  wheels: Wheel[];
+
+  // TODO: Create a constructor that accepts the properties of the Motorbike class
+    // TODO: The constructor should call the constructor of the parent class, Vehicle
+    // TODO: The constructor should initialize the properties of the Motorbike class
+    // TODO: The constructor should check if the wheels array has 2 elements and create 2 new default Wheel objects if it does not
+   
+    constructor(
+      vin: string,
+      color: string,
+      make: string,
+      model: string,
+      year: number,
+      weight: number,
+      topSpeed: number,
+      wheels: Wheel[]
+    ) {
+   
+    super(vin, color, make, model, year, weight, topSpeed);
+    this.wheels = wheels.length === 2 ? wheels : [new Wheel(17, 'DefaultBrand'), new Wheel(17, 'DefaultBrand')];
+  }
+
+  // TODO: Implement the wheelie method
+    // TODO: The method should log the message "Motorbike [make] [model] is doing a wheelie!"
+    
+  wheelie(): void {
+    console.log(`Motorbike ${this.make} ${this.model} is doing a wheelie!`);
+  }
+
+  // TODO: Override the printDetails method from the Vehicle class
+  // TODO: The method should call the printDetails method of the parent class
+  // TODO: The method should log the details of the Motorbike
+  // TODO: The details should include the VIN, make, model, year, weight, top speed, color, and wheels
+  printDetails(): void {
+    super.printDetails(); // Call the parent class method
+    console.log(`Wheels: Front - ${this.wheels[0].diameter}" (${this.wheels[0].brand}), Rear - ${this.wheels[1].diameter}" (${this.wheels[1].brand})`);
+  }
+}
+
+// Export the Motorbike class as the default export
+export default Motorbike;
